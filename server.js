99d72aa68be4863049b1ee8287be797246ba7d90
David April <dsa157@gmail.com>
2025-05-16 14:41:50 +0200

    Correct production data directory path

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const serverless = require('serverless-http');
 
 const app = express();
 const DATA_DIR = process.env.NETLIFY
-  ? path.join(process.cwd(), 'data')
+  ? path.join(process.cwd(), '..', 'public/data')
   : path.join(__dirname, 'public/data');
 
-debugData('Using data directory: %s', DATA_DIR);
+debugData('Using data directory: %s (exists: %s)', DATA_DIR, fs.existsSync(DATA_DIR));
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
